4ce57aa719281972a7e5c58a3bc7fb911f9b836d
Muhammad Taufique Hussain <taufique71@gmail.com>
2016-06-29 19:17:52 +0600

    Test cases added for variable count

---
+++ test/variable_count/cases/case_3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int a;
+
+int main(){
+    printf("Hello World!");
+    return 0;
+}
---
+++ test/variable_count/cases/case_4.c
@@ -0,0 +1,9 @@
+int func(int b){
+    return b;
+}
+
+int main(){
+    int c = 1, d = 2;
+    c = func(d);
+    return 0;
+}
