d4855af51d935f5e91b8d3a8c8b2540e97c7d1c2
Muhammad Taufique Hussain <taufique71@gmail.com>
2016-06-30 18:06:43 +0600

    Current tests made asynchronous

--- test/variable_count/variable_count.js
+++ test/variable_count/variable_count.js
@@ -9,39 +9,70 @@ describe('variable_count', function() {
         assert(typeof(variable_count), "function");
     });
 
-    it("Should have 0 variables", function(){
+    it("Should have 0 variables", function(done){
         var variable_count = require("../../lib/variable_count").variable_count;
         var file = __dirname + '/cases/case_1.json';
         jsonfile.readFile(file, function(err, obj) {
             var n = variable_count(obj);
             expect(n).to.equal(0);
+            done();
         });
     });
 
-    it("Should have 0 variables", function(){
+    it("Should have 0 variables", function(done){
         var variable_count = require("../../lib/variable_count").variable_count;
         var file = __dirname + '/cases/case_2.json';
         jsonfile.readFile(file, function(err, obj) {
             var n = variable_count(obj);
             expect(n).to.equal(0);
+            done();
         });
     });
 
-    it("Should have 1 variables", function(){
+    it("Should have 1 variables", function(done){
         var variable_count = require("../../lib/variable_count").variable_count;
         var file = __dirname + '/cases/case_3.json';
         jsonfile.readFile(file, function(err, obj) {
             var n = variable_count(obj);
-            expect(n).to.deep.equal(0);
-            assert.strictEqual(n, 0);
+            expect(n).to.equal(1);
+            done();
+            //assert.strictEqual(n, 0);
         });
     });
-    it("Should have 3 variables", function(){
+    it("Should have 3 variables", function(done){
         var variable_count = require("../../lib/variable_count").variable_count;
         var file = __dirname + '/cases/case_4.json';
         jsonfile.readFile(file, function(err, obj) {
             var n = variable_count(obj);
             expect(n).to.equal(3);
+            done();
+        });
+    });
+    it("Should have 3 variables", function(done){
+        var variable_count = require("../../lib/variable_count").variable_count;
+        var file = __dirname + '/cases/case_5.json';
+        jsonfile.readFile(file, function(err, obj) {
+            var n = variable_count(obj);
+            expect(n).to.equal(3);
+            done();
+        });
+    });
+    it("Should have 2 variables", function(done){
+        var variable_count = require("../../lib/variable_count").variable_count;
+        var file = __dirname + '/cases/case_6.json';
+        jsonfile.readFile(file, function(err, obj) {
+            var n = variable_count(obj);
+            expect(n).to.equal(2);
+            done();
+        });
+    });
+    it("Should have 3 variables", function(done){
+        var variable_count = require("../../lib/variable_count").variable_count;
+        var file = __dirname + '/cases/case_7.json';
+        jsonfile.readFile(file, function(err, obj) {
+            var n = variable_count(obj);
+            expect(n).to.equal(3);
+            done();
         });
     });
 });
