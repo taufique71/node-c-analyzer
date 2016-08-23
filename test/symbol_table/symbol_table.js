45ca30e23bc44f28a6c20a7ca70ba1d823148045
Muhammad Taufique Hussain <taufique71@gmail.com>
2016-08-23 17:45:37 +0600

    Differentiating function scopes from global scope done.

--- test/symbol_table/symbol_table.js
+++ test/symbol_table/symbol_table.js
@@ -2,20 +2,20 @@ var assert = require("chai").assert;
 var expect = require("chai").expect;
 var jsonfile = require("jsonfile");
 
-describe('variable_count', function() {
-    it('Should be able to require `variable_count` as a function', function () {
-        var variable_count = require("../../lib/symbol_table").symbol_table;
-        assert(variable_count);
-        assert(typeof(variable_count), "function");
+describe('Symbol table', function() {
+    it('Should be able to require `get_symbol_table` as a function', function () {
+        var get_symbol_table = require("../../lib/symbol_table").get_symbol_table;
+        assert(get_symbol_table);
+        assert(typeof(get_symbol_table), "function");
     });
 
-    it("Should have 0 variables", function(done){
-        var symbol_table = require("../../lib/symbol_table").symbol_table;
+    it("Should return valid symbol table", function(done){
+        var get_symbol_table = require("../../lib/symbol_table").get_symbol_table;
         var add_parent_edges = require("../../lib/utility").add_parent_edges;
         var file = __dirname + '/cases/case_8.json';
         jsonfile.readFile(file, function(err, obj) {
-            console.log(JSON.stringify(obj));
-            symbol_table(obj);
+            //console.log(JSON.stringify(obj));
+            get_symbol_table(obj);
             done();
         });
     });
