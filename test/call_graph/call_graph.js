4ba68ccb17add514aca230d3e970450cff284843
Md. Taufique Hussain <taufique@muktosoft.com>
2016-11-22 19:51:22 +0600

    Clears unit test directory.

--- test/call_graph/call_graph.js
+++ test/call_graph/call_graph.js
@@ -8,41 +8,4 @@ describe('Call graph', function() {
         assert(get_call_graph);
         assert(typeof(get_call_graph), "function");
     });
-
-    it("Should return valid call graph", function(done){
-        var get_call_graph = require("../../lib/call_graph").get_call_graph;
-        var file = __dirname + '/cases/case_1.json';
-        jsonfile.readFile(file, function(err, obj) {
-            var graph = get_call_graph(obj);
-            //console.log(graph);
-            done();
-        });
-    });
-    it("Should return valid call graph", function(done){
-        var get_call_graph = require("../../lib/call_graph").get_call_graph;
-        var file = __dirname + '/cases/case_2.json';
-        jsonfile.readFile(file, function(err, obj) {
-            var graph = get_call_graph(obj);
-            //console.log(graph);
-            done();
-        });
-    });
-    it("Should return valid call graph", function(done){
-        var get_call_graph = require("../../lib/call_graph").get_call_graph;
-        var file = __dirname + '/cases/case_3.json';
-        jsonfile.readFile(file, function(err, obj) {
-            var graph = get_call_graph(obj);
-            //console.log(graph);
-            done();
-        });
-    });
-    it("Should return valid call graph", function(done){
-        var get_call_graph = require("../../lib/call_graph").get_call_graph;
-        var file = __dirname + '/cases/case_4.json';
-        jsonfile.readFile(file, function(err, obj) {
-            var graph = get_call_graph(obj);
-            //console.log(graph);
-            done();
-        });
-    });
 });
--- test/call_graph/cases/case_1.c
+++
@@ -1,8 +0,0 @@
-void func(void){
-    return;
-}
-
-int main(){
-    func();
-	return 0;
-}
